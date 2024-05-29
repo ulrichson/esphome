f696db9eef7ffee268ae8d0659b70271eea7fc4d
Ulrich Lehner <me@ulrichlehner.com>
2024-05-29 16:10:05 +0200

    fix: attempt

--- components/model-railroad/__init__.py
+++ components/model-railroad/__init__.py
@@ -1,12 +1,16 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
 
-from esphome.const import (CONF_ID)
+DEPENDENCIES = []
+
+my_custom_component_ns = cg.esphome_ns.namespace('modelrailroad')
+ModelRailroadComponent = my_custom_component_ns.class_('ModelRailroadComponent', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ModelRailroadComponent),
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
-
-    var = cg.new_Pvariable(condition_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
