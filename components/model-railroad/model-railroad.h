b02edb9c4109312efb44ba13e49d292b3308392b
Ulrich Lehner <me@ulrichlehner.com>
2024-05-30 17:32:22 +0200

    improvement: basic

--- components/model-railroad/model-railroad.h
+++ components/model-railroad/model-railroad.h
@@ -1,10 +1,10 @@
 #include "esphome.h"
 
-#define LED_PIN0 0
-#define LED_PIN1 1;
-#define LED_PIN2 2;
-#define LED_PIN3 3;
-#define DAMP_RANDOM_BUFFER_LENGTH 128;
+// #define LED_PIN0 0
+// #define LED_PIN1 1;
+// #define LED_PIN2 2;
+// #define LED_PIN3 3;
+// #define DAMP_RANDOM_BUFFER_LENGTH 128;
 
 namespace esphome
 {
@@ -14,29 +14,30 @@ namespace esphome
     class ModelRailroadComponent : public Component
     {
     public:
-      bool enable = true;
-      float dampFactor = 0.15f; // 0 .. 1, higher is more damped;
-      int minIntensity = 160;
+      // bool enable = true;
+      // float dampFactor = 0.15f; // 0 .. 1, higher is more damped;
+      // int minIntensity = 160;
 
       ModelRailroadComponent() {}
 
       void setup() override
       {
-        pinMode(LED_PIN0, OUTPUT);
-        pinMode(LED_PIN1, OUTPUT);
-        pinMode(LED_PIN2, OUTPUT);
-        pinMode(LED_PIN3, OUTPUT);
-        digitalWrite(LED_PIN0, LOW);
-        digitalWrite(LED_PIN1, LOW);
-        digitalWrite(LED_PIN2, LOW);
-        digitalWrite(LED_PIN3, LOW);
-        for (int i = 0; i < 4; i++)
-        {
-          for (int j = 0; j < DAMP_RANDOM_BUFFER_LENGTH; j++)
-          {
-            buffer[i][j] = 0;
-          }
-        }
+        // pinMode(LED_PIN0, OUTPUT);
+        // pinMode(LED_PIN1, OUTPUT);
+        // pinMode(LED_PIN2, OUTPUT);
+        // pinMode(LED_PIN3, OUTPUT);
+        // digitalWrite(LED_PIN0, LOW);
+        // digitalWrite(LED_PIN1, LOW);
+        // digitalWrite(LED_PIN2, LOW);
+        // digitalWrite(LED_PIN3, LOW);
+        // for (int i = 0; i < 4; i++)
+        // {
+        //   for (int j = 0; j < DAMP_RANDOM_BUFFER_LENGTH; j++)
+        //   {
+        //     buffer[i][j] = 0;
+        //   }
+        // }
+        pinMode(2, OUTPUT);
       }
 
       void loop() override
@@ -58,50 +59,49 @@ namespace esphome
         delay(500);
       }
 
-    private:
-      int buffer[4][DAMP_RANDOM_BUFFER_LENGTH];
-      int bufferIdx = 0;
+      // private:
+      //   int buffer[4][DAMP_RANDOM_BUFFER_LENGTH];
+      //   int bufferIdx = 0;
 
-      int setLight(int pin, int idx)
-      {
-        int intensity = int((avg(buffer[idx]) * dampFactor + float(random(minIntensity, 255)) * (1 - dampFactor)) / 2);
+      //   int setLight(int pin, int idx)
+      //   {
+      //     int intensity = int((avg(buffer[idx]) * dampFactor + float(random(minIntensity, 255)) * (1 - dampFactor)) / 2);
 
-        analogWrite(pin, intensity);
-        pop(idx);
-        push(intensity, idx);
+      //     analogWrite(pin, intensity);
+      //     pop(idx);
+      //     push(intensity, idx);
 
-        return intensity;
-      }
+      //     return intensity;
+      //   }
 
-      boolean push(int element, int idx)
-      {
-        if (bufferIdx < DAMP_RANDOM_BUFFER_LENGTH)
-        {
-          buffer[idx][bufferIdx++] = element;
-          return true;
-        }
+      //   boolean push(int element, int idx)
+      //   {
+      //     if (bufferIdx < DAMP_RANDOM_BUFFER_LENGTH)
+      //     {
+      //       buffer[idx][bufferIdx++] = element;
+      //       return true;
+      //     }
 
-        return false;
-      }
+      //     return false;
+      //   }
 
-      int pop(int idx)
-      {
-        if (bufferIdx > 0)
-          return buffer[idx][--bufferIdx];
-        else
-          return 0;
-      }
+      //   int pop(int idx)
+      //   {
+      //     if (bufferIdx > 0)
+      //       return buffer[idx][--bufferIdx];
+      //     else
+      //       return 0;
+      //   }
 
-      float avg(int values[])
-      {
-        int sum = 0;
-        for (int i = 0; i < DAMP_RANDOM_BUFFER_LENGTH; i++)
-        {
-          sum += values[i];
-        }
-        return sum / DAMP_RANDOM_BUFFER_LENGTH;
-      }
-    };
-
+      //   float avg(int values[])
+      //   {
+      //     int sum = 0;
+      //     for (int i = 0; i < DAMP_RANDOM_BUFFER_LENGTH; i++)
+      //     {
+      //       sum += values[i];
+      //     }
+      //     return sum / DAMP_RANDOM_BUFFER_LENGTH;
+      //   }
+      // };
+    }
   }
-}
