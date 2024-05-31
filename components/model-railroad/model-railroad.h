fedab8170db34d81884be3c41e7480b0948b8bf9
Ulrich Lehner <me@ulrichlehner.com>
2024-05-31 11:11:18 +0200

    improvement: non-blocking loop

--- components/model-railroad/model-railroad.h
+++ components/model-railroad/model-railroad.h
@@ -1,10 +1,10 @@
 #include "esphome.h"
 
-// #define LED_PIN0 0
-// #define LED_PIN1 1;
-// #define LED_PIN2 2;
-// #define LED_PIN3 3;
-// #define DAMP_RANDOM_BUFFER_LENGTH 128;
+#define LED_PIN0 0
+#define LED_PIN1 1;
+#define LED_PIN2 2;
+#define LED_PIN3 3;
+#define DAMP_RANDOM_BUFFER_LENGTH 128;
 
 namespace esphome
 {
@@ -14,30 +14,30 @@ namespace esphome
     class ModelRailroadComponent : public Component
     {
     public:
-      // bool enable = true;
-      // float dampFactor = 0.15f; // 0 .. 1, higher is more damped;
-      // int minIntensity = 160;
+      bool enable = true;
+      float dampFactor = 0.15f; // 0 .. 1, higher is more damped;
+      int minIntensity = 160;
 
       ModelRailroadComponent() {}
 
       void setup() override
       {
-        // pinMode(LED_PIN0, OUTPUT);
-        // pinMode(LED_PIN1, OUTPUT);
-        // pinMode(LED_PIN2, OUTPUT);
-        // pinMode(LED_PIN3, OUTPUT);
-        // digitalWrite(LED_PIN0, LOW);
-        // digitalWrite(LED_PIN1, LOW);
-        // digitalWrite(LED_PIN2, LOW);
-        // digitalWrite(LED_PIN3, LOW);
-        // for (int i = 0; i < 4; i++)
-        // {
-        //   for (int j = 0; j < DAMP_RANDOM_BUFFER_LENGTH; j++)
-        //   {
-        //     buffer[i][j] = 0;
-        //   }
-        // }
-        pinMode(2, OUTPUT);
+        pinMode(LED_PIN0, OUTPUT);
+        pinMode(LED_PIN1, OUTPUT);
+        pinMode(LED_PIN2, OUTPUT);
+        pinMode(LED_PIN3, OUTPUT);
+        digitalWrite(LED_PIN0, LOW);
+        digitalWrite(LED_PIN1, LOW);
+        digitalWrite(LED_PIN2, LOW);
+        digitalWrite(LED_PIN3, LOW);
+        for (int i = 0; i < 4; i++)
+        {
+          for (int j = 0; j < DAMP_RANDOM_BUFFER_LENGTH; j++)
+          {
+            buffer[i][j] = 0;
+          }
+        }
+        // pinMode(2, OUTPUT);
       }
 
       void loop() override
@@ -47,61 +47,69 @@ namespace esphome
         //   return;
         // }
 
-        // setLight(LED_PIN0, 0);
-        // setLight(LED_PIN1, 1);
-        // setLight(LED_PIN2, 2);
-        // setLight(LED_PIN3, 3);
-        // delay(1000 /* ms */ / 30 /* fps */);
+        currentMs = millis();
+        if (currentMs - previousMs >= intervalMs)
+        {
+          previousMs = currentMs;
+          setLight(LED_PIN0, 0);
+          setLight(LED_PIN1, 1);
+          setLight(LED_PIN2, 2);
+          setLight(LED_PIN3, 3);
+        }
 
-        digitalWrite(2, LOW);
-        delay(500);
-        digitalWrite(2, HIGH);
-        delay(500);
+        // digitalWrite(2, LOW);
+        // delay(500);
+        // digitalWrite(2, HIGH);
+        // delay(500);
       }
 
-      // private:
-      //   int buffer[4][DAMP_RANDOM_BUFFER_LENGTH];
-      //   int bufferIdx = 0;
+    private:
+      int buffer[4][DAMP_RANDOM_BUFFER_LENGTH];
+      int bufferIdx = 0;
 
-      //   int setLight(int pin, int idx)
-      //   {
-      //     int intensity = int((avg(buffer[idx]) * dampFactor + float(random(minIntensity, 255)) * (1 - dampFactor)) / 2);
+      unsigned long currentMs = 0;
+      unsigned long previousMs = 0;
+      const long intervalMs = 1000 /* ms */ / 30 /* fps */;
 
-      //     analogWrite(pin, intensity);
-      //     pop(idx);
-      //     push(intensity, idx);
+      int setLight(int pin, int idx)
+      {
+        int intensity = int((avg(buffer[idx]) * dampFactor + float(random(minIntensity, 255)) * (1 - dampFactor)) / 2);
 
-      //     return intensity;
-      //   }
+        analogWrite(pin, intensity);
+        pop(idx);
+        push(intensity, idx);
 
-      //   boolean push(int element, int idx)
-      //   {
-      //     if (bufferIdx < DAMP_RANDOM_BUFFER_LENGTH)
-      //     {
-      //       buffer[idx][bufferIdx++] = element;
-      //       return true;
-      //     }
+        return intensity;
+      }
 
-      //     return false;
-      //   }
+      boolean push(int element, int idx)
+      {
+        if (bufferIdx < DAMP_RANDOM_BUFFER_LENGTH)
+        {
+          buffer[idx][bufferIdx++] = element;
+          return true;
+        }
 
-      //   int pop(int idx)
-      //   {
-      //     if (bufferIdx > 0)
-      //       return buffer[idx][--bufferIdx];
-      //     else
-      //       return 0;
-      //   }
+        return false;
+      }
 
-      //   float avg(int values[])
-      //   {
-      //     int sum = 0;
-      //     for (int i = 0; i < DAMP_RANDOM_BUFFER_LENGTH; i++)
-      //     {
-      //       sum += values[i];
-      //     }
-      //     return sum / DAMP_RANDOM_BUFFER_LENGTH;
-      //   }
+      int pop(int idx)
+      {
+        if (bufferIdx > 0)
+          return buffer[idx][--bufferIdx];
+        else
+          return 0;
+      }
+
+      float avg(int values[])
+      {
+        int sum = 0;
+        for (int i = 0; i < DAMP_RANDOM_BUFFER_LENGTH; i++)
+        {
+          sum += values[i];
+        }
+        return sum / DAMP_RANDOM_BUFFER_LENGTH;
+      }
     };
   }
 }
