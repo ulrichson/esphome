b0c30ab35b359fa2da76a3798d5d074dec5aaed7
Ulrich Lehner <me@ulrichlehner.com>
2024-06-03 15:09:21 +0200

    fix: attempt

--- esphome/components/modelrailway/__init__.py
+++ esphome/components/modelrailway/__init__.py
@@ -1,40 +1,24 @@
+from esphome import pins
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components.light import LightState
-from esphome.const import CONF_ID, CONF_OUTPUT_ID
-
 from esphome.components import light
+from esphome.const import CONF_OUTPUT_ID
+from .. import status_led_ns
 
-MULTI_CONF = False
-
-AUTO_LOAD = [ "light" ]
-
-DEPENDENCIES = []
+AUTO_LOAD = ["output"]
 
 modelrailway_ns = cg.esphome_ns.namespace('modelrailway')
 ModelRailwayComponent = modelrailway_ns.class_('ModelRailwayComponent', light.LightOutput, cg.Component)
 
-CONF_LIGHT = "light"
-
-# CONFIG_SCHEMA = light.BINARY_LIGHT_SCHEMA.extend(
-#     {
-#         # cv.GenerateID(CONF_ID): cv.declare_id(LightState),
-#         # cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(ModelRailwayComponent)
-#         cv.GenerateID(): cv.declare_id(ModelRailwayComponent),
-#     }
-# ).extend(cv.COMPONENT_SCHEMA)
-
-CONFIG_SCHEMA = light.BINARY_LIGHT_SCHEMA.extend(
-    {
-        cv.GenerateID(CONF_ID): cv.declare_id(LightState),
-        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(ModelRailwayComponent)
-        # cv.GenerateID(): cv.declare_id(ModelRailwayComponent),
-    }
-).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.All(
+    light.BINARY_LIGHT_SCHEMA.extend(
+        {
+            cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(ModelRailwayComponent)
+        }
+    )
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
-    # var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
     await light.register_light(var, config)
-    # cg.add(cg.App.register_light(var))
-    # await cg.register_component(var, config)
