56b5e5d0680292e8ba877539af3eeb73948de2af
Ulrich Lehner <me@ulrichlehner.com>
2024-06-03 09:59:44 +0200

    fix: attempt

--- esphome/components/modelrailway/__init__.py
+++ esphome/components/modelrailway/__init__.py
@@ -16,16 +16,24 @@ ModelRailwayComponent = modelrailway_ns.class_('ModelRailwayComponent', light.Li
 
 CONF_LIGHT = "light"
 
+# CONFIG_SCHEMA = light.BINARY_LIGHT_SCHEMA.extend(
+#     {
+#         # cv.GenerateID(CONF_ID): cv.declare_id(LightState),
+#         # cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(ModelRailwayComponent)
+#         cv.GenerateID(): cv.declare_id(ModelRailwayComponent),
+#     }
+# ).extend(cv.COMPONENT_SCHEMA)
+
 CONFIG_SCHEMA = light.BINARY_LIGHT_SCHEMA.extend(
     {
         # cv.GenerateID(CONF_ID): cv.declare_id(LightState),
         # cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(ModelRailwayComponent)
         cv.GenerateID(): cv.declare_id(ModelRailwayComponent),
     }
-).extend(cv.COMPONENT_SCHEMA)
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    cg.add(cg.App.register_light(var))
-    await cg.register_component(var, config)
-    # await light.register_light(var, config)
+    # cg.add(cg.App.register_light(var))
+    # await cg.register_component(var, config)
+    await light.register_light(var, config)
