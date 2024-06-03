541d38663e2321ad8529b6468c28e6b124f547b7
Ulrich Lehner <me@ulrichlehner.com>
2024-06-03 12:19:45 +0200

    fix: attempt

--- esphome/components/modelrailway/__init__.py
+++ esphome/components/modelrailway/__init__.py
@@ -34,6 +34,6 @@ CONFIG_SCHEMA = light.BINARY_LIGHT_SCHEMA.extend(
 
 async def to_code(config):
     # var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
-    # await light.register_light(var, config)
     var = cg.new_Pvariable(config[CONF_ID])
+    await light.register_light(var, config)
     await cg.register_component(var, config)
